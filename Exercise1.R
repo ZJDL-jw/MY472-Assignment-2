1938ecfb2167c9990ecac5080e71a400c110adc6
wjy <wenjiayu26@outlook.com>
2023-11-02 14:37:42 +0000

    Added new functionality to the R function.

--- Exercise1.R
+++ Exercise1.R
@@ -1,15 +1,19 @@
-# Define a function to read and transform the data from a CSV file
-transform_data <- function(file_path) {
-  # Read the CSV file into a data frame
-  data <- read.csv(file_path)
-  
-  # Calculate the average income for each age group
-  result <- aggregate(income ~ age, data, FUN = mean)
+# Modified function on the "dev" branch
+transform_data_dev <- function(data, calc_method = "average") {
+  if (calc_method == "average") {
+    # Calculate the average income for each age group
+    result <- aggregate(income ~ age, data, FUN = mean)
+  } else if (calc_method == "total") {
+    # Calculate the total income for each age group
+    result <- aggregate(income ~ age, data, FUN = sum)
+  } else {
+    stop("Invalid calculation method. Use 'average' or 'total'.")
+  }
   
   # Rename the columns for clarity
-  colnames(result) <- c("age", "average_income")
+  colnames(result) <- c("age", ifelse(calc_method == "average", "average_income", "total_income"))
   
   return(result)
 }
 
-transform_data("Exercise1_data.csv")
+
